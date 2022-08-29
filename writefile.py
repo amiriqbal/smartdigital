9ea34bd94fd57c881a953749617c54592bce5d3d
local_admin <local_admin@lhind-n210026.local>
2022-08-29 03:53:45 +0200

    update file with Berlin as City

--- writefile.py
+++ writefile.py
@@ -1,7 +1,7 @@
 # Program to show various ways to read and
 # write data in a file.
 file1 = open("myfile.txt","w")
-L = ["This is Delhi \n","This is Paris \n","This is London \n"]
+L = ["This is Berlin \n","This is Paris \n","This is London \n"]
 
 # \n is placed to indicate EOL (End of Line)
 file1.write("Hello \n")
